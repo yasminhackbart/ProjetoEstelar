fe1e5b72da55a2b504ac5d46bb5d30b6861fc73a
Yasmin Hackbart <90115526+yasminhackbart@users.noreply.github.com>
2022-08-30 14:18:12 -0300

    Update App.js

--- PROC76AppEstelar/App.js
+++ PROC76AppEstelar/App.js
@@ -16,7 +16,7 @@ function App() {
       <Stack.Navigator initialRouteName="Home" screenOptions={{
         headerShown: false
       }}>
-        <Stack.Screen name="Home" component={HomeScreen} />
+ 
         <Stack.Screen name="StarMap" component={StartMapScreen} />
         <Stack.Screen name="DailyPic" component={DailyPicScreen} />
         <Stack.Screen name="SpaceCraft" component={SpaceCraftScreen} />
